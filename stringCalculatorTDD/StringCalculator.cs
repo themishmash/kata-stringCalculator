e872ba4d3eb07c2b158a48c62e9731aaec05ae0b
Michelle <wong.mich.c@gmail.com>
2020-03-25 14:59:31 +1100

    test 5 complete

--- stringCalculatorTDD/StringCalculator.cs
+++ stringCalculatorTDD/StringCalculator.cs
@@ -4,8 +4,26 @@ namespace stringCalculatorTDD
     {
         public int Add(string input)
         {
-            int result;
-            int.TryParse(input, out result);
+            
+            int result; //default of int is 0
+
+            if (input == string.Empty)
+            {
+                return 0;
+            }
+            if (int.TryParse(input, out result)) //single input 
+            {
+                return result;
+            }
+
+         
+            char[] delimiter = {',', '\n' };
+            string[] splitInput = input.Split(delimiter);
+            foreach (string letter in splitInput)
+            {
+                result += int.Parse(letter);
+            }
+
             return result;
         }
     }
--- stringCalculatorTests/StringCalculatorTests.cs
+++ stringCalculatorTests/StringCalculatorTests.cs
@@ -20,11 +20,41 @@ namespace stringCalculatorTests
         public void AddNumberReturnsNumber()
         {
             var stringCalculator = new StringCalculator();
-            var result = stringCalculator.Add("1");
             
-            Assert.AreEqual(1,result);
+            Assert.AreEqual(1,stringCalculator.Add("1"));
+            Assert.AreEqual(3,stringCalculator.Add("3"));
         }
-        
-        
+
+        [Test]
+        public void AddTwoNumbersReturnsSum()
+        {
+            var stringCalculator = new StringCalculator();
+
+            Assert.AreEqual(3, stringCalculator.Add("1,2"));
+            Assert.AreEqual(8, stringCalculator.Add("3,5"));
+        }
+
+        [Test]
+        public void AddAnyNumbersReturnsSum()
+        {
+            var stringCalculator = new StringCalculator();
+            Assert.AreEqual(6, stringCalculator.Add("1,2,3"));
+            Assert.AreEqual(20, stringCalculator.Add("3,5,3,9"));
+        }
+
+        [Test]
+        public void AddAnyNumbersReturnsSumWithLineBreaks()
+        {
+            var stringCalculator = new StringCalculator();
+            Assert.AreEqual(6, stringCalculator.Add("1,2\n3"));
+            Assert.AreEqual(20, stringCalculator.Add("3\n5\n3,9"));
+        }
+
+        [Test]
+        public void AddDifferentDelimitersReturnsSum()
+        {
+            var stringCalculator = new StringCalculator();
+            Assert.AreEqual(3, stringCalculator.Add("//;\n1;2"));
+        }
     }
 }
